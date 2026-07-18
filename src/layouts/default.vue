440d9b73448a21d8b19358b52b8617f40d5d77f4
Van Chin <van.chin@qq.com>
2026-07-18 17:59:08 +0800

    fix: main content padding-bottom to prevent tabbar overlay
    - van-tabbar is position:fixed, add padding-bottom to main
    - Use Vant CSS variable --van-tabbar-height for accurate sizing
    - Only apply when showFooter is active

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="layout-default bd-red box-border flex min-h-screen flex-col bg-gray-50">
     <DefaultHeader v-if="showHeader" />
-    <main class="bd-red min-h-10 flex-1 px-2">
+    <main class="bd-red min-h-10 flex-1 px-2" :class="{ 'pb-tabbar': showFooter }">
       <router-view />
     </main>
     <DefaultFooter v-if="showFooter" />
@@ -16,3 +16,11 @@ defineOptions({ name: 'LayoutDefault' });
 
 const { showHeader, showFooter } = useLayoutConfig();
 </script>
+
+<style scoped>
+/* van-tabbar 默认 position: fixed，脱离文档流。
+   给 main 添加等高的 padding-bottom，避免内容被 tabbar 遮挡。 */
+.pb-tabbar {
+  padding-bottom: var(--van-tabbar-height, 50px);
+}
+</style>
