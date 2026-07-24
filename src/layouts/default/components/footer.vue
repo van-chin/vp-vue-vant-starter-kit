2720d930fe999af21db9384d426d24efa7dc08c3
Van Chin <van.chin@qq.com>
2026-07-24 17:38:30 +0800

    wip: add pages & layouts

--- src/layouts/default/components/footer.vue
+++ src/layouts/default/components/footer.vue
@@ -5,12 +5,12 @@
       当前布局为 h-screen flex-col，footer 固定在视口底部，
       无需 fixed，回归正常流让 flex 自动分配空间，清除 padding-bottom hack。
     -->
-    <van-tabbar v-model="active" :fixed="false">
-      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
-      <van-tabbar-item icon="list-switching">分类</van-tabbar-item>
-      <van-tabbar-item icon="fire-o" badge="5">种草</van-tabbar-item>
-      <van-tabbar-item icon="cart-o" badge="20">购物车</van-tabbar-item>
-      <van-tabbar-item icon="contact-o" badge="6">我的</van-tabbar-item>
+    <van-tabbar v-model="active" :fixed="false" @change="onChange">
+      <van-tabbar-item icon="home-o" name="home" to="/">首页</van-tabbar-item>
+      <van-tabbar-item icon="list-switching" name="category" to="/categories">分类</van-tabbar-item>
+      <van-tabbar-item icon="fire-o" badge="5" name="seed" to="/seed">种草</van-tabbar-item>
+      <van-tabbar-item icon="cart-o" badge="20" name="cart" to="/cart">购物车</van-tabbar-item>
+      <van-tabbar-item icon="contact-o" badge="6" name="my" to="/my">我的</van-tabbar-item>
     </van-tabbar>
   </footer>
 </template>
@@ -19,4 +19,7 @@
 defineOptions({ name: 'DefaultFooter' });
 
 const active = ref(0);
+const onChange = (key: string) => {
+  console.info('onChange => key', key);
+};
 </script>
---
+++ src/pages/cart.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="index">
+    <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+      <van-swipe-item>1</van-swipe-item>
+      <van-swipe-item>2</van-swipe-item>
+      <van-swipe-item>3</van-swipe-item>
+      <van-swipe-item>4</van-swipe-item>
+    </van-swipe>
+    <van-nav-bar title="seed" />
+    <div class="bd-red">cart</div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <div class="bd-red h-200">index</div>
+
+    <Department></Department>
+    <div class="">
+      <div class="bd-red">11bottom</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+definePage({
+  meta: {
+    // layout: 'default',
+    showHeader: true,
+    showFooter: true,
+  },
+});
+</script>
+
+<style scoped>
+.my-swipe .van-swipe-item {
+  color: #fff;
+  font-size: 20px;
+  line-height: 150px;
+  text-align: center;
+  background-color: #39a9ed;
+}
+</style>
